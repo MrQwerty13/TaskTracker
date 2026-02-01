eced34cfc1c39473d871db9c1100d12c198e1cb7
MrQwerty13 <pozur07@icloud.com>
2026-02-01 11:01:48 +0300

    Delete UseCases directory

--- UseCases/delete_task.py
+++
@@ -1,9 +0,0 @@
-from Interfaces.task_repository import TaskRepository
-
-
-class DeleteTaskCase:
-    def __init__(self, repository: TaskRepository):
-        self.repository = repository
-
-    def execute(self, task_id: str) -> None:
-        self.repository.remove(task_id)
